920afda094d50853ecb6823aaee27a146c35e2bd
anggoletomi <anggoletomi@gmail.com>
2024-11-24 22:01:52 +0700

    adding sync drive to workflow

--- rc_report/main_load_db.py
+++ rc_report/main_load_db.py
@@ -6,12 +6,15 @@ sys.path.insert(0, os.getenv("PROJECT_PATH"))
 
 from bi_function import log_function
 
+from rc_gdrive_data_sync import main_drive_to_local
 from sp_income_released import sp_income_released
 from sp_order_data import sp_order_data
 from sp_pay_wallet import sp_pay_wallet
 
 
 tasks = [
+    (main_drive_to_local, {}),
+
     (sp_income_released, {'count_file': 100,'db_method': 'replace'}),
     (sp_order_data, {'count_file': 100,'db_method': 'replace'}),
     (sp_pay_wallet, {'count_file': 100,'db_method': 'replace'}),
